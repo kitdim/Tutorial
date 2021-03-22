2a3f4157fa15d376ddca44a9335b9c035b6fe6cc
Дима <kitaykin9808@gmail.com>
2021-03-22 19:29:40 +0300

    Добавьте файлы проекта.

---
+++ Kata_Sheeps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Kata_Sheeps
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Метод для подсчета истины 
+        /// </summary>
+        static int Sheps(bool[] a)/// <summary>
+                                  /// Метод для подсчета истины 
+                                  /// </summary>
+                                  /// <param name="a"></param>
+                                  /// <returns></returns>
+        {
+            int b = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == true) b++;
+            }
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            var sheeps = new bool[] { true, false, true };
+            Console.WriteLine(Sheps(sheeps));
+            Console.ReadKey();
+        }
+    }
+}
