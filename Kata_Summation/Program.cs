6fbedbd55b32fd76d218cce6e275cee7ef6c1ef9
Дима <kitaykin9808@gmail.com>
2021-03-22 19:47:13 +0300

    добавил кату на нахождение суммы

---
+++ Kata_Summation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kata_Summation
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод для подсчета суммы
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int summation(int num)
+        {
+            var sum = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                sum += i;
+
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(summation(100)); Console.ReadKey();
+        }
+    }
+}
