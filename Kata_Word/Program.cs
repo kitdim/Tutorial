51d201a46f48670f95625f3359327fb5929fead0
Дима <kitaykin9808@gmail.com>
2021-03-30 15:44:32 +0300

    Осталось подумать над выводом и удалением ненужных индексов

---
+++ Kata_Word/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kata_Word
+{
+    class Program
+    {
+        public static int[] Capitals(string word)
+        {
+            int a = word.Length;
+            int[] ar = new int[a];
+            int i = 0 , b = 0;
+
+            while (i<word.Length)
+            {
+                if (word[i]==word.ToUpper()[i])
+                {
+                  ar[b] = i;
+                  b++;
+                }
+                i++;
+            }
+            return ar;
+
+        }
+        static void Main(string[] args)
+        {
+            string a = "SsSsS";
+            Console.WriteLine(Capitals(a));
+            Console.ReadKey();
+        }
+    }
+}
