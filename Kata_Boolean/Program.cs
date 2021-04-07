241f15489bf897793bd79d442e8ebc3053d892fb
Дима <kitaykin9808@gmail.com>
2021-04-07 14:57:09 +0300

    Boolean

---
+++ Kata_Boolean/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kata_Boolean
+{
+    class Program
+    {
+        /// <summary>
+        /// Перевод логического значения в строку
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static string false_true(bool b)
+        {
+            string TrueOrFalse;
+
+            if (b == true) return TrueOrFalse = "True";
+            else return TrueOrFalse = "False";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(false_true(true));
+            Console.WriteLine(false_true(false));
+            Console.ReadKey();
+        }
+    }
+}
