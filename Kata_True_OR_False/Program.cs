572549f4751f964459d70419fda5d7136e0b2fdc
Дима <kitaykin9808@gmail.com>
2021-04-22 12:20:35 +0300

    +

---
+++ Kata_True_OR_False/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kata_True_OR_False
+{
+    class Program
+    {
+        static string boolToWord(bool word)
+        {
+            string bw = word.ToString();
+
+            if (bw == "True") return bw = "Yes";
+            else return bw = "No";
+        }
+        static void Main(string[] args)
+        {
+            boolToWord(true);
+            boolToWord(false);
+            Console.ReadKey();
+        }
+    }
+}
