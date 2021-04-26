3c1df714ce52c88d07862e3552a81b3d447c5d9d
Дима <kitaykin9808@gmail.com>
2021-04-26 16:36:05 +0300

    Нахождение суммы 2 минимальных чисел

---
+++ Kata_One-Two/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata_One_Two
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод для поиска двух минимальных чисел в массиве
+        /// </summary>
+        /// <param name="ar"></param>
+        /// <returns></returns>
+        public static int SumTwoMin(int[] ar)
+        {
+            int OneMin = ar.Min();
+            int TwoMin = ar[1];
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (TwoMin > ar[i] && ar[i] != OneMin) TwoMin = ar[i];
+            }
+
+            return OneMin + TwoMin;
+        }
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 0, 4, 5 };
+            int sum = SumTwoMin(ar);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
